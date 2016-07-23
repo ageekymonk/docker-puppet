149c3ccb37b5607ac8ec2e8b33b17deb67e2bdaa
ramz <ramzthecoder@gmail.com>
2016-07-23 16:01:57 +1000

    Updating with right folder

--- puppetserver/debian/wheezy/docker-entrypoint.d/00-copy-folder-structure.sh
+++ puppetserver/debian/wheezy/docker-entrypoint.d/00-copy-folder-structure.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-[ "$(ls -A /etc/puppetlabs)" ] && echo "Not Empty" || cp -Rf /etc/puppetlabs-data/* /etc/puppetlabs/.
+[ "$(ls -A /etc/puppetlabs)" ] && echo "Not Empty" || cp -Rf /etc/puppetlabs-template/* /etc/puppetlabs/.
