b0364cc626eeb387d2a025acd59b13125e77fb91
ramz <ramzthecoder@gmail.com>
2016-07-23 16:34:44 +1000

    Updating with JAVA_ARGS default value

--- puppetserver/debian/wheezy/docker-entrypoint.d/02-set-java-args.sh
+++ puppetserver/debian/wheezy/docker-entrypoint.d/02-set-java-args.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-
-sed -i "s/^JAVA_ARGS.*/JAVA_ARGS=\"${JAVA_ARGS}\"/g" /etc/default/puppetserver
+if ![ -z ${JAVA_ARGS} ];
+then
+    sed -i "s/^JAVA_ARGS.*/JAVA_ARGS=\"${JAVA_ARGS}\"/g" /etc/default/puppetserver
+fi
